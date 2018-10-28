b7ac9dc264e7a654d22bfa73907cd32ad327cd33
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-10-28 09:48:47 +0530

    Histogram calculated

---
+++ ESIHEscratch.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import sys
+#import flatten-dict
+#import mcat
+
+from PIL import Image
+#import pillow as plt
+
+# Load the image into an array: image
+image = Image.open('abhi.jpg')
+image.load()
+data = np.asarray(image, dtype="int32")
+print("Matrix:", data);
+hist,bins = np.histogram(data.ravel(),256,[0,256])
+print (hist) #Histogram of the image calculated
--- HistoClassification1.py
+++ HistoClassification1.py
@@ -1,82 +1,40 @@
-import matplotlib
-import matplotlib.pyplot as plt
+
 import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import sys
+#import flatten-dict
+#import mcat
 
-from skimage import data, img_as_float
-from skimage import exposure
+from PIL import Image
+#import pillow as plt
 
+# Load the image into an array: image
+image = Image.open('abhi.jpg')
+image.load()
+data = np.asarray(image, dtype="int32")
+print("Matrix:", data);
 
-matplotlib.rcParams['font.size'] = 8
+# Flatten the image into 1 dimension: pixels
+pixels = data.flatten()
 
+# Generate a cumulative histogram
+cdf, bins, patches = plt.hist(pixels, bins=256, range=(0,256), normed=True, cumulative=True)
+new_pixels = np.interp(pixels, bins[:-1], cdf*255)
 
-def plot_img_and_hist(image, axes, bins=256):
-    """Plot an image along with its histogram and cumulative histogram.
+# Reshape new_pixels as a 2-D array: new_image
+new_image = new_pixels.reshape(data.shape)
 
-    """
-    image = img_as_float(image)
-    ax_img, ax_hist = axes
-    ax_cdf = ax_hist.twinx()
+# Display the new image with 'gray' color map
+plt.subplot(2,1,1)
+plt.title('Equalized image')
+plt.axis('off')
+plt.imshow(new_image, cmap='gray')
 
-    # Display image
-    ax_img.imshow(image, cmap=plt.cm.gray)
-    ax_img.set_axis_off()
+a1=np.asarray( np.clip(new_image,0,255), dtype="uint8")
 
-    # Display histogram
-    ax_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
-    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
-    ax_hist.set_xlabel('Pixel intensity')
-    ax_hist.set_xlim(0, 1)
-    ax_hist.set_yticks([])
+img = Image.fromarray( a1, "RGB" )
+img.show()
 
-    # Display cumulative distribution
-    img_cdf, bins = exposure.cumulative_distribution(image, bins)
-    ax_cdf.plot(bins, img_cdf, 'r')
-    ax_cdf.set_yticks([])
-
-    return ax_img, ax_hist, ax_cdf
-
-
-# Load an example image
-img = data.moon()
-
-# Contrast stretching
-p2, p98 = np.percentile(img, (2, 98))
-img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
-
-# Equalization
-img_eq = exposure.equalize_hist(img)
-
-# Adaptive Equalization
-img_adapteq = exposure.equalize_adapthist(img, clip_limit=0.03)
-
-# Display results
-fig = plt.figure(figsize=(8, 5))
-axes = np.zeros((2, 4), dtype=np.object)
-axes[0, 0] = fig.add_subplot(2, 4, 1)
-for i in range(1, 4):
-    axes[0, i] = fig.add_subplot(2, 4, 1+i, sharex=axes[0,0], sharey=axes[0,0])
-for i in range(0, 4):
-    axes[1, i] = fig.add_subplot(2, 4, 5+i)
-
-ax_img, ax_hist, ax_cdf = plot_img_and_hist(img, axes[:, 0])
-ax_img.set_title('Low contrast image')
-
-y_min, y_max = ax_hist.get_ylim()
-ax_hist.set_ylabel('Number of pixels')
-ax_hist.set_yticks(np.linspace(0, y_max, 5))
-
-ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_rescale, axes[:, 1])
-ax_img.set_title('Contrast stretching')
-
-ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_eq, axes[:, 2])
-ax_img.set_title('Histogram equalization')
-
-ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_adapteq, axes[:, 3])
-ax_img.set_title('Adaptive equalization')
-
-ax_cdf.set_ylabel('Fraction of total intensity')
-ax_cdf.set_yticks(np.linspace(0, 1, 5))
-
-# prevent overlap of y-axis labels
-fig.tight_layout()
-plt.show();
+outimg = Image.fromarray( new_image,"RGB" )
+outimg.show();
