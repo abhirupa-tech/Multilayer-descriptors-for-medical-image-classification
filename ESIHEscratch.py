42a98b5b10f20a104ce585ef4e931315824c3a3a
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-11-06 11:34:29 +0530

    Clipping threshold and clipped hst calculated

--- ESIHEscratch.py
+++ ESIHEscratch.py
@@ -15,6 +15,30 @@ image.load()
 data = np.asarray(image, dtype="int32")
 print("Matrix:", data);
 hist,bins = np.histogram(data.ravel(),256,[0,256])
-printI"Histogram of the image:")
+print("Histogram of the image:")
 print (hist) #Histogram of the image calculate
+
+#Step2, calculate Xa
+num =0
+denum=0
+l=len(hist);
+for i in range(1,l+1) :
+    num+= hist[i-1]*i
+    denum+=hist[i-1]
+
+#Step3 Clipping threshold and calculate clipping hstogram
+exposure = (1.0/l)*(num/(1.0*denum))
+xa=l*(1-exposure)
+print("Exposure value:", exposure)
+print("Threshold value:", xa)
+clipthreshold =0
+
+s=0
+for i in range(1,l+1):
+   s+=hist[i-1]
+clipthreshold = (1.0/l)*s
+
+for i in range(1, l+1):
+    if(hist[k]>clipthreshold):
+        hist[k]=clipthreshold
 
